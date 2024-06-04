379bb7760cf914c9688d4ad42ef06fde88226a66
Taytay884 <itay@joindaisy.com>
2024-06-04 12:50:32 +0300

    fixed CORS issue

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,16 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 initMongoConnection().catch(() => shutdown());
 initRoutes(app);
 
