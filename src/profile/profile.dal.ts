b39fb6b37b929510e0bd1715c234291762e3a479
Taytay884 <itay@joindaisy.com>
2024-08-30 15:42:40 +0300

    fix get profile by id

--- src/profile/profile.dal.ts
+++ src/profile/profile.dal.ts
@@ -7,7 +7,70 @@ export async function createProfile(profile: IProfile): Promise<IProfile> {
 }
 
 export async function getProfileById(id: string): Promise<IProfile | null> {
-  return Profile.findById({ id }).populate({ path: 'courses', foreignField: 'id' });
+  const profileWithMatches = await Profile.aggregate([
+    { $match: { id } },
+    {
+      $lookup: {
+        from: 'matches', // The collection name for matches
+        let: { profileId: '$_id' },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $or: [
+                  { $eq: ['$tutor', '$$profileId'] },
+                  { $eq: ['$student', '$$profileId'] },
+                ],
+              },
+            },
+          },
+          {
+            $lookup: {
+              from: 'profiles', // The collection name for profiles
+              localField: 'tutor',
+              foreignField: '_id',
+              as: 'tutorProfile',
+            },
+          },
+          {
+            $lookup: {
+              from: 'profiles', // The collection name for profiles
+              localField: 'student',
+              foreignField: '_id',
+              as: 'studentProfile',
+            },
+          },
+          {
+            $addFields: {
+              tutorProfile: { $arrayElemAt: ['$tutorProfile', 0] },
+              studentProfile: { $arrayElemAt: ['$studentProfile', 0] },
+            },
+          },
+          {
+            $project: {
+              tutor: '$tutorProfile',
+              student: '$studentProfile',
+              hoursRequested: 1,
+              hoursApproved: 1,
+              dateMatched: 1,
+              status: 1,
+            },
+          },
+        ],
+        as: 'matches',
+      },
+    },
+    {
+      $lookup: {
+        from: 'courses', // The collection name for courses
+        localField: 'courses',
+        foreignField: 'id',
+        as: 'courses',
+      },
+    },
+  ]);
+
+  return profileWithMatches.length > 0 ? profileWithMatches[0] : null;
 }
 
 export async function getProfiles(filter: GetProfilesFilter): Promise<IProfile[]> {
